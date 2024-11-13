466c6872aacfc1d3b98ab34fc6e210aae6f99bce
Vladica-Stojic-BIT <vladica.stojic@bit.admin.ch>
2024-11-13 16:15:42 +0100

    BitNexusMavenHostedRepo configured

--- didresolver-android/build.gradle.kts
+++ didresolver-android/build.gradle.kts
@@ -64,8 +64,26 @@ configure<PublishingExtension> {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/e-id-admin/didresolver-kotlin-android")
             credentials {
+                // For the GitHub packages, create a personal access token having 'write:packages' scope
+                // and then store it into gradle.properties:
+                // gpr.user=<your_GitHub_user>
+                // gpr.token=****************************************
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+            }
+        }
+        maven {
+            name = "BitNexusMavenHostedRepo"
+            url = uri("https://nexus.bit.admin.ch/repository/bj-swiyu-maven-hosted")
+            credentials {
+                // For the BIT Nexus Maven repo, login onto https://nexus.bit.admin.ch with your u808***** account.
+                // Ensure your U-account already have writing grants for the repo (bj-swiyu-maven-hosted).
+                // Create an access user token (visit https://nexus.bit.admin.ch/#user/usertoken)
+                // and then store it into gradle.properties:
+                // nexus.user=********
+                // nexus.token=********************************************
+                username = project.findProperty("nexus.user") as String? ?: System.getenv("NEXUS_USERNAME")
+                password = project.findProperty("nexus.token") as String? ?: System.getenv("NEXUS_TOKEN")
             }
         }
     }
