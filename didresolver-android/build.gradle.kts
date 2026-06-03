2e25a52e3393442455cc24fac049d3fdf312b3c4
Marcel Pfeuti <marcel.pfeuti@bit.admin.ch>
2026-06-03 10:34:25 +0200

    chore: update version to 2.9.0-SNAPSHOT

--- didresolver-android/build.gradle.kts
+++ didresolver-android/build.gradle.kts
@@ -117,7 +117,7 @@ mavenPublishing {
         //         For the sake of Maven Central publishing, it must now match the relevant Maven Central namespace
         "ch.admin.swiyu.didresolver",
         "didresolver-android",
-        "2.7.0")
+        "2.9.0-SNAPSHOT")
 
     pom {
         name = "DID Resolver (Kotlin/Android)"
