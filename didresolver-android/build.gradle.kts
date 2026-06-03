97e8f81482a612f53a80bfe3df38de9ce895d7cb
Marcel Pfeuti <marcel.pfeuti@bit.admin.ch>
2026-06-03 10:33:53 +0200

    chore: update version to 2.8.0

--- didresolver-android/build.gradle.kts
+++ didresolver-android/build.gradle.kts
@@ -117,7 +117,7 @@ mavenPublishing {
         //         For the sake of Maven Central publishing, it must now match the relevant Maven Central namespace
         "ch.admin.swiyu.didresolver",
         "didresolver-android",
-        "2.7.0")
+        "2.8.0")
 
     pom {
         name = "DID Resolver (Kotlin/Android)"
